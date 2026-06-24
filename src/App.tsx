a67ddc199209edcfd6888a62d6db586b4f5f1c8d
Ayushi jain <ayushislaptop@Ayushis-MacBook-Air-2.local>
2026-06-24 23:14:48 +0530

    working ai chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
+import Widget from "./components/Widget/Widget";
+
 function App() {
-  return (
-    <div
-      style={{
-        position: "fixed",
-        bottom: "20px",
-        right: "20px",
-        width: "60px",
-        height: "60px",
-        background: "red",
-        borderRadius: "50%",
-        zIndex: 999999,
-      }}
-    >
-      AI
-    </div>
-  );
+  return <Widget />;
 }
 
 export default App;
--- src/widget/bootstrap.ts
+++ src/widget/bootstrap.ts
@@ -2,24 +2,23 @@ import { createShadowRoot } from "./shadowRoot";
 import { mountReactApp } from "./mount";
 
 export const bootstrapWidget = () => {
-  console.log("Bootstrap Started");
-
-  if (document.getElementById("ai-chatbot-widget")) {
+  // prevent duplicate injection
+  if (
+    document.getElementById(
+      "ai-chatbot-widget"
+    )
+  ) {
     return;
   }
 
   const host = document.createElement("div");
+
   host.id = "ai-chatbot-widget";
 
   document.body.appendChild(host);
 
-  console.log("Host Added");
-
-  const shadowRoot = createShadowRoot(host);
-
-  console.log("Shadow Created");
+  const shadowRoot =
+    createShadowRoot(host);
 
   mountReactApp(shadowRoot);
-
-  console.log("React Mounted");
 };
