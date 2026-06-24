67b8aa761ebe95f133c97806c9638fdf4617c5ba
Ayushi jain <ayushislaptop@Ayushis-MacBook-Air-2.local>
2026-06-24 22:23:59 +0530

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import Widget from "./components/Widget/Widget";
-
 function App() {
-  return <Widget />;
+  return (
+    <div
+      style={{
+        position: "fixed",
+        bottom: "20px",
+        right: "20px",
+        width: "60px",
+        height: "60px",
+        background: "red",
+        borderRadius: "50%",
+        zIndex: 999999,
+      }}
+    >
+      AI
+    </div>
+  );
 }
 
 export default App;
